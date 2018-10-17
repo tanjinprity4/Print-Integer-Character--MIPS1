0f160f21a3a655f6c575e3518ec97d79674e09bc
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-10-16 22:07:15 -0400

    load and print 0 after @

--- PrintByIntCharacter.asm
+++ PrintByIntCharacter.asm
@@ -2,10 +2,14 @@
 .text						# Assembly language instructions
 main:						# Start of code section
 	li $v0, 11				# system call code for printing character = 11
-	li $a0, '@'				#load the ascii code for '@'
-	syscall
+	li $a0, '@'				# load the ascii character '@' = 64
+	syscall					# call operating system to perform operation
 	
+	li $v0, 1				# system call code for printing integer = 1
+	addi $a0, $a0, -64		# stored integer 0 in $a0 adding 64 to old value of $ao
+	syscall					 			
+
 	li $v0, 10				# system call code for exit = 10
-	syscall
+	syscall					# Exit program
    
    
