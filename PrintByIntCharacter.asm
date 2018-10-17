fa6617a6390c9e4b6ba4077efdd4a28f18f5cf93
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-10-16 21:38:24 -0400

    started the program in an assembly recognizable file

---
+++ PrintByIntCharacter.asm
@@ -0,0 +1,7 @@
+.data
+.text                       	# Assembly language instructions
+main:                           # Start of code section    	
+	li $v0, 10		            # system call code for exit = 10
+	syscall
+   
+   
