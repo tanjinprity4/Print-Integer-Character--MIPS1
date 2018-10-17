1f9c4488bd792cfc5e018ec10ca8756f667130d9
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-10-16 21:44:14 -0400

    load and print ascii character '@'

--- PrintByIntCharacter.asm
+++ PrintByIntCharacter.asm
@@ -1,7 +1,11 @@
 .data
-.text                       	# Assembly language instructions
-main:                           # Start of code section    	
-	li $v0, 10		            # system call code for exit = 10
+.text						# Assembly language instructions
+main:						# Start of code section
+	li $v0, 11				# system call code for printing character = 11
+	li $a0, '@'				#load the ascii code for '@'
+	syscall
+	
+	li $v0, 10				# system call code for exit = 10
 	syscall
    
    
