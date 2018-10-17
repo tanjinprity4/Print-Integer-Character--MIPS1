5d5f4a8a0b49b933167fd16c60ef8d6172876deb
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-10-16 23:14:00 -0400

    calculated value to load and print ascii characters 'n' and 'j'

--- PrintByIntCharacter.asm
+++ PrintByIntCharacter.asm
@@ -47,6 +47,11 @@ main:						# Start of code section
 	syscall
 	addi $a0, $a0, 13			# 84 + 13 = 97; load ascii character 'a' in $a0
 	syscall
+	addi $a0, $a0, 13			# 97 + 13 = 110; load ascii character 'n' in $a0
+	syscall
+	addi $a0, $a0, -4			# 110 + (-4) = 106; load ascii character 'j' in $a0
+	syscall
+
 	
 	li $v0, 10				# system call code for exit = 10
 	syscall					# Exit program
