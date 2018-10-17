1f00c1d4def1b95cec5334b03003a7a6c307f4d6
Tanjin Prity <tanjin@Tanjins-MacBook-Pro.local>
2018-10-16 22:28:52 -0400

    calculated value to store and print integers 3, 5, and 9

--- PrintByIntCharacter.asm
+++ PrintByIntCharacter.asm
@@ -18,6 +18,11 @@ main:						# Start of code section
 	syscall
 	addi $a0, $a0, -3			# 6 + (-3) = 3; store 3 in $a0
 	syscall
+	syscall					# print 3
+	addi $a0, $a0, 2			# 3 + 2 = 5; store 5 in $a0
+	syscall					#print 5
+	addi $a0, $a0, 4			# 5 + 4 = 9; store 5 in $a0
+	syscall					# print 9
 	
 	li $v0, 10				# system call code for exit = 10
 	syscall					# Exit program
